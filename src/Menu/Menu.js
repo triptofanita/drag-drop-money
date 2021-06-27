acaa9802092197c1f4ac528c004e12db7f193397
Talia Gonzalez <triptofanita@Talias-MacBook-Pro.local>
2021-06-26 20:46:03 -0500

    Complementary styles are added for menu component

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -12,13 +12,16 @@ const handleFirstLevel = () => {
 }
 
     return (
-        <section> 
-            <Instructions/>
+        <section class="menu__main--container"> 
+            <h1 className="menu__title">Pagando en la tiendita</h1>
             <div className="menu__container">
-                <button onClick={handleFirstLevel} className="option first__level">Nivel 1</button>
-                <button className="option second__level">Nivel 2</button>
-                <button className="option third__level">Nivel 3</button>
-                <button className="option fourth__level">Nivel 4</button>
+            <Instructions/>
+                <div className="menu__container--options">
+                    <button onClick={handleFirstLevel} className="option first__level">Nivel 1</button>
+                    <button className="option second__level">Nivel 2</button>
+                    <button className="option third__level">Nivel 3</button>
+                    <button className="option fourth__level">Nivel 4</button>
+                </div>   
             </div>
         </section>
     )
